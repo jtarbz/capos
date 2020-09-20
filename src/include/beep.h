652c9a4836935d917dc2ada0e93c97e9118b5579
Jason Walter <jaason@protonmail.ch>
2020-09-20 12:41:47 -0400

    Revert "world's most popular song, now right in your kernelgit add ."
    This reverts commit e701f25c83f4ced02216f774e53ef3ded765a4ce.

--- src/include/beep.h
+++ src/include/beep.h
@@ -6,7 +6,5 @@
 void beep(uint32_t freq);
 void unbeep(void);
 void tbeep(uint32_t freq, uint32_t ms);
-void beep_two(uint32_t freq1, uint32_t freq2, uint32_t ms);
-void beep_three(uint32_t freq1, uint32_t freq2, uint32_t freq3, uint32_t ms);
 
 #endif
